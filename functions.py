29e329f5d7c16836b56a7eb191df71982c912962
ameso213 <142228988+ameso213@users.noreply.github.com>
2023-10-11 14:40:25 +0100

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,29 @@
+#write a python program to print the even numbers from a given list[1,2,3,4,5,6,7,8,9]
+def even_numbers_in_list(r,s,t,u,v,w,x,y,z):
+    even_list=[s,u,w,y]
+    print(even_list)
+even_numbers_in_list(1,2,3,4,5,6,7,8,9)    
+    
+
+#write a python function program to sum all the numbers in a list [8,2,3,0,7]
+def sum_of_all_numbers(a,b,c,d,e):
+    output=a+b+c+d+e
+    print(f"the sum of all numbers {a},{b},{c},{d},{e},is {output},")
+sum_of_all_numbers(8,2,3,0,7)  
+
+
+#write a python program to reverse a string.
+#1,2,3,4,a,b,c,d
+string="1,2,3,4,a,b,c,d"
+print(string[::-1])
+    
+    
+#write a python function to multiply all numbers in list=[8,2,3,-1,7] 
+    
+def multiplication_of_all_numbers(p,b,c,d,e):
+        output=p*b*c*d*e
+        print(f"multiplication of {p},{b},{c},{d}and{e} is{output}")
+multiplication_of_all_numbers(8,2,3,-1,7)    
+      
+          
+  
